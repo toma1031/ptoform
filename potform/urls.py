fef5d8c0e0760d7fae8293daf520fb6060bdec3a
Akinori Toma <akinoritoma@appletoma.home>
2021-03-30 00:45:01 -0400

    lemplement search box

--- potform/urls.py
+++ potform/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from potform import views
-from .views import (IndexView, EmployeeView, SupervisorView, HrView, SuccessView, LoginView, ApprovedRequestView, DeclinedRequestView, ApprovedRequestDetailView, DeclinedRequestDetailView)
+from .views import (IndexView, EmployeeView, SupervisorView, HrView, SuccessView, LoginView, ApprovedRequestView, DeclinedRequestView, ApprovedRequestDetailView, EmployeeApprovedRequestView,EmployeeApprovedRequestDetailView,DeclinedRequestDetailView, EmployeeDeclinedRequestView,EmployeeDeclinedRequestDetailView)
 
 urlpatterns = [
     path('', IndexView.as_view()),
@@ -9,9 +9,15 @@ urlpatterns = [
     path('hr/', HrView.as_view(), name='hr'),
     path('success/', SuccessView.as_view(), name='success'),
     path('login/', LoginView.as_view(), name='login'),
+    # approve
     path('approved_request_hr/', ApprovedRequestView.as_view(), name='approved_request_hr'),
-    path('declined_request_hr/', DeclinedRequestView.as_view(), name='declined_request_hr'),
     path('approved_request_hr_detail/<int:pk>/', ApprovedRequestDetailView.as_view(), name='approved_request_hr_detail'),
+    path('approved_request_employee/', EmployeeApprovedRequestView.as_view(), name='approved_request_employee'),
+    path('approved_request_employee_detail/<int:pk>/', EmployeeApprovedRequestDetailView.as_view(), name='approved_request_employee_detail'),
+    # decline
+    path('declined_request_hr/', DeclinedRequestView.as_view(), name='declined_request_hr'),
     path('declined_request_hr_detail/<int:pk>/', DeclinedRequestDetailView.as_view(), name='declined_request_hr_detail'),
+    path('declined_request_employee/', EmployeeDeclinedRequestView.as_view(), name='declined_request_employee'),
+    path('declined_request_employee_detail/<int:pk>/', EmployeeDeclinedRequestDetailView.as_view(), name='declined_request_employee_detail'),
 
 ]
--- potform/views.py
+++ potform/views.py
@@ -52,6 +52,7 @@ class EmployeeView(LoginRequiredMixin, CreateView):
 class SupervisorView(LoginRequiredMixin, ListView):
     context_object_name = 'request_list'
     model = RequestPTO
+    paginate_by = 8
     template_name = "supervisor.html"
 
     def get(self, request, *args, **kwargs):
@@ -77,6 +78,7 @@ class HrView(LoginRequiredMixin, ListView):
     model = RequestPTO
     queryset = RequestPTO.objects.filter(request=1)
     template_name = "hr.html"
+    paginate_by = 8
 
     def get(self, request, *args, **kwargs):
         user =  User.objects.filter(username=self.request.user)
@@ -98,6 +100,7 @@ class ApprovedRequestView(LoginRequiredMixin, ListView):
     model = RequestPTO
     queryset = RequestPTO.objects.filter(request=4)
     template_name = "approved_request_hr.html"
+    paginate_by = 8
 
     def get(self, request, *args, **kwargs):
         user =  User.objects.filter(username=self.request.user)
@@ -105,6 +108,16 @@ class ApprovedRequestView(LoginRequiredMixin, ListView):
             return super().get(request, **kwargs)
         else:
             return redirect('index')
+
+    def get_queryset(self):
+        q_request_date_from = self.request.GET.get('query')
+        
+        if q_request_date_from:
+            object_list = RequestPTO.objects.filter(
+                Q(request_date_from__icontains=q_request_date_from))
+        else:
+            object_list = RequestPTO.objects.filter(request=4)
+        return object_list.filter(request=4)
 
 class ApprovedRequestDetailView(LoginRequiredMixin, DetailView):
     context_object_name = 'request'
@@ -119,10 +132,51 @@ class ApprovedRequestDetailView(LoginRequiredMixin, DetailView):
         else:
             return redirect('index')
 
+class EmployeeApprovedRequestView(LoginRequiredMixin, ListView):
+    context_object_name = 'request_list'
+    model = RequestPTO
+    queryset = RequestPTO.objects.filter(request=4)
+    paginate_by = 8
+    template_name = "approved_request_employee.html"
+
+    def get(self, request, *args, **kwargs):
+        user =  User.objects.filter(username=self.request.user)
+        if user and user[0].is_employee:
+            return super().get(request, **kwargs)
+        else:
+            return redirect('index')
+
+    def get_queryset(self):
+        # return RequestPTO.objects.filter(request=4,post_employee=self.request.user)
+
+        q_request_date_from = self.request.GET.get('query')
+        
+        if q_request_date_from:
+            object_list = RequestPTO.objects.filter(
+                Q(request_date_from__icontains=q_request_date_from))
+        else:
+            object_list = RequestPTO.objects.filter(request=4, post_employee=self.request.user)
+        return object_list.filter(request=4, post_employee=self.request.user)
+
+
+class EmployeeApprovedRequestDetailView(LoginRequiredMixin, DetailView):
+    context_object_name = 'request'
+    model = RequestPTO
+    queryset = RequestPTO.objects.filter(request=4)
+    template_name = "approved_request_employee_detail.html"
+
+    def get(self, request, *args, **kwargs):
+        user =  User.objects.filter(username=self.request.user)
+        if user and user[0].is_employee:
+            return super().get(request, **kwargs)
+        else:
+            return redirect('index')
+
 class DeclinedRequestView(LoginRequiredMixin, ListView):
     context_object_name = 'request_list'
     model = RequestPTO
     queryset = RequestPTO.objects.filter(request=2)
+    paginate_by = 8
     template_name = "declined_request_hr.html"
 
     def get(self, request, *args, **kwargs):
@@ -132,6 +186,15 @@ class DeclinedRequestView(LoginRequiredMixin, ListView):
         else:
             return redirect('index')
 
+    def get_queryset(self):
+        q_request_date_from = self.request.GET.get('query')
+        
+        if q_request_date_from:
+            object_list = RequestPTO.objects.filter(
+                Q(request_date_from__icontains=q_request_date_from))
+        else:
+            object_list = RequestPTO.objects.filter(request=2)
+        return object_list.filter(request=2)
 
 class DeclinedRequestDetailView(LoginRequiredMixin, DetailView):
     context_object_name = 'request'
@@ -145,14 +208,52 @@ class DeclinedRequestDetailView(LoginRequiredMixin, DetailView):
             return super().get(request, **kwargs)
         else:
             return redirect('index')
-    def post(self, request, *args, **kwargs):
 
-        if  self.request.POST['request'] == 'Decline':
-            queryset = RequestPTO.objects.filter(id=self.request.POST['id']).update(request=4)
+
+class EmployeeDeclinedRequestView(LoginRequiredMixin, ListView):
+    context_object_name = 'request_list'
+    model = RequestPTO
+    queryset = RequestPTO.objects.filter(request=2)
+    paginate_by = 8
+    template_name = "declined_request_employee.html"
+
+    def get(self, request, *args, **kwargs):
+        user =  User.objects.filter(username=self.request.user)
+        if user and user[0].is_employee:
+            return super().get(request, **kwargs)
         else:
-            queryset = RequestPTO.objects.filter(id=self.request.POST['id']).update(request=2)
-        return redirect('hr')
+            return redirect('index')
+    
+    def get_queryset(self):
+        # return RequestPTO.objects.filter(request=2,post_employee=self.request.user)
 
+        q_request_date_from = self.request.GET.get('query')
+        
+        if q_request_date_from:
+            object_list = RequestPTO.objects.filter(
+                Q(request_date_from__icontains=q_request_date_from))
+        else:
+            object_list = RequestPTO.objects.filter(request=2, post_employee=self.request.user)
+        return object_list.filter(request=2, post_employee=self.request.user)
+
+class EmployeeDeclinedRequestDetailView(LoginRequiredMixin, DetailView):
+    context_object_name = 'request'
+    model = RequestPTO
+    queryset = RequestPTO.objects.filter(request=2)
+    template_name = "declined_request_employee_detail.html"
+
+    def get(self, request, *args, **kwargs):
+        user =  User.objects.filter(username=self.request.user)
+        if user and user[0].is_employee:
+            return super().get(request, **kwargs)
+        else:
+            return redirect('index')
+    
+    def post(self, request, *args, **kwargs):
+        if  self.request.POST['request'] == 'Resubmit':
+            queryset = RequestPTO.objects.filter(id=self.request.POST['id']).update(request=3)
+            print(self.request.POST)
+        return redirect('success')
 
 class SuccessView(LoginRequiredMixin, TemplateView):
     template_name = "success.html"
