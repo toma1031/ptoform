9a595fe624ca5d60ce3328f1a4301aff205a00bb
Akinori Toma <akinoritoma@appletoma.home>
2021-05-18 02:49:23 -0400

    Reedit requirements.txt for heroku3

--- website/settings_mysql.py
+++ website/settings_mysql.py
@@ -22,8 +22,8 @@ config_ini = configparser.ConfigParser()
 config_ini_path = 'config.ini'
 
 # 指定したiniファイルが存在しない場合、エラー発生
-if not os.path.exists(config_ini_path):
-    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), config_ini_path)
+# if not os.path.exists(config_ini_path):
+#     raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), config_ini_path)
 
 config_ini.read(config_ini_path, encoding='utf-8')
 
